964518bbc7a7ff731a32b88fcd83d627f65287c4
sleepyhoon <pokjm99@naver.com>
2024-11-21 17:27:20 +0900

    feat : save Transaction in LinkedHashMap

---
+++ src/main/java/com/repository/TransactionRepository.java
@@ -0,0 +1,42 @@
+package com.repository;
+
+import com.domain.entity.Transaction;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+
+/**
+ * <br>package name   : com.repository
+ * <br>file name      : TransactionRepository
+ * <br>date           : 2024-11-21
+ * <pre>
+ * <span style="color: white;">[description]</span>
+ *
+ * </pre>
+ * <pre>
+ * <span style="color: white;">usage:</span>
+ * {@code
+ *
+ * } </pre>
+ * <pre>
+ * modified log :
+ * =======================================================
+ * DATE           AUTHOR               NOTE
+ * -------------------------------------------------------
+ * 2024-11-21        SeungHoon              init create
+ * </pre>
+ */
+public class TransactionRepository {
+    private final LinkedHashMap<String, Transaction> linkedHashMap = new LinkedHashMap<>();
+
+    public void save(Transaction transaction) {
+        linkedHashMap.put(transaction.getTxId(),transaction);
+    }
+
+    public Transaction get(String id) {
+        return linkedHashMap.get(id);
+    }
+
+    public Collection<Transaction> findAll() {
+        return linkedHashMap.values();
+    }
+}
